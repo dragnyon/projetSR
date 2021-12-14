b5375a1bfe11f41a43d8b6acbda799493392f572
dragnyon <81358334+dragnyon@users.noreply.github.com>
2021-12-14 11:10:01 +0100

    Add files via upload

--- serveur.c
+++ serveur.c
@@ -5,6 +5,9 @@
 #include <string.h>
 #include <pthread.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+
 
 pthread_mutex_t mutex= PTHREAD_MUTEX_INITIALIZER;
 struct sockaddr_in serv; 
@@ -14,32 +17,35 @@ char message[100] = "";
 char test[100]="vous etes connecte\0";
 int nbjoueur=0;
 int joueur[5];
-pid_t pid;
+int manche=1;
 
-
+//int cartesjoueurs[nbjoueur][manche+1];
+//int cartejouer[manche*nbjoueur];
 
 void afficheconn();
 void* envinfo(void* info);
+//void topdepart();
+//void initcarte();
 void main()
 {
 	printf("Démarrage du jeu...\n");
 	serv.sin_family = AF_INET;
-	serv.sin_port = htons(1234); //Define the port at which the server will listen for connections.
+	serv.sin_port = htons(1234); 
 	serv.sin_addr.s_addr = INADDR_ANY;
 	fd = socket(AF_INET, SOCK_STREAM, 0);
 	bind(fd, (struct sockaddr *)&serv, sizeof(serv));
-	listen(fd,3);
-
+	listen(fd,5);
+	
+		
 	while(1)
 	{
+				
 		
 		conn = accept(fd, (struct sockaddr *)NULL, NULL);
-		
-		nbjoueur++; 
 		joueur[nbjoueur]=conn;
-		
+		nbjoueur++; 
 		afficheconn();
-		pid=fork();
+	
 		pthread_t threadenv;
 		pthread_create(&threadenv,NULL,envinfo,(void*)&message);
 		
@@ -65,11 +71,8 @@ void afficheconn()
 void* envinfo(void* info)
 {
 	while(1)
-	{
-		
-		if(pid==0)
-		{
-			
+	{		
+		printf("%d joueurs - %d %d %d %d %d",nbjoueur,joueur[0],joueur[1],joueur[2],joueur[3],joueur[4]);
 				if(recv(conn, message, 100, 0)>1)
 				{
 						printf("\nCartes jouer par le joueur %d: %s\n",conn, message);  
@@ -80,13 +83,36 @@ void* envinfo(void* info)
     					memset(message,0,sizeof(message));
 				}
 			
-		}
+		
 	}
 
 }
 
+/*
+void topdepart()
+{
+	int temps=0;
+	time_t begin=time(NULL);
+	printf("debut compteur");
+	while(temps<10) {
 
+	
+	sleep(1);
+	
+	temps=(int)difftime(end,begin);
+	if(temps<10) {
+		printf("%d",temps);
+		
+	}
+	else {
+	
+		printf("temps ecoule");		
+		exit(1);
+	}
+}
 
+}
+*/
 
 
 
